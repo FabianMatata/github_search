c4fc1a83fd9e436eb628c082441c8d0515920711
Fabian Matata <mfabian601@gmail.com>
2022-01-17 09:01:41 +0300

    add files

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  registerUserData: any ={};
+  profile:any[];
+  repos: any[];
+  // bio: any[];
+  username:string;
+  // company:string;
+  company: any[];
+  location:string;
+
+  constructor(private profileService: ProfileService) { 
+
+  }
+
+  findProfile(){
+  	this.profileService.updateProfile(this.username);
+  	this.profileService.getProfileInfo().subscribe((profile:any) => {
+  		console.log(profile);
+  		this.profile = profile;
+  	});
+  
+  	this.profileService.getProfileRepos().subscribe((repos:any) => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})  	
+  }
+
+  ngOnInit() {
+  }
+
+}
